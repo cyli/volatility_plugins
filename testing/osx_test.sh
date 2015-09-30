d80c36a68e044444836282a24f170095794a6ff4
cyli <cyli@twistedmatrix.com>
2015-09-29 23:49:56 -0700

    Ok, move where volatility and the virtualenv is installed because volatility apparently does weird import scanning of the plugin subdirectory

--- testing/osx_test.sh
+++ testing/osx_test.sh
@@ -9,13 +9,19 @@
 
 # --- Download and set up volatility ---
 if [[ ! -d "volatility" ]]; then
-    git clone -b 2.4.1 --single-branch https://github.com/volatilityfoundation/volatility.git
+    git clone --single-branch https://github.com/volatilityfoundation/volatility.git volatility
+else
+    echo "Volatility master has already been downloaded"
 fi
 
 if [[ ! -d ".vol_venv" ]]; then
     virtualenv .vol_venv
     source .vol_venv/bin/activate
-    pip install volatility/
+    pip install distorm3 pycrypto
+    pip install -e volatility/
+else
+    echo "Volatility virtualenv already set up"
+    source .vol_venv/bin/activate
 fi
 
 
@@ -23,29 +29,38 @@ fi
 if [[ "$(uname)" == 'Darwin' ]]; then
    export PATH="${PATH}:/Applications/VMware Fusion.app/Contents/Library"
    PROVIDER=vmware_fusion
+else
+    echo "Currently only OS X with VMWare Fusion is supported."
+    exit 1
 fi
 
 
 # --- Build the profile if it's not there ---
 if [[ ! -f "volatility/volatility/plugins/overlays/linux/Ubuntu1404.zip" ]]; then
-    vagrant up profile --provider="${PROVIDER}"
+    if [[ -z "$(vagrant status profile | grep 'not created')" ]]; then
+        vagrant up profile
+        vagrant provision profile
+    else
+        vagrant up profile --provider="${PROVIDER}"
+    fi
     vagrant suspend profile
+else
+    echo "Linux profile for Ubuntu 14.04 already exists."
 fi
 
-# --- Set up the memory testing box ---
-# if [[ "$(uname)" == 'Darwin' ]]; then
-#    export PATH="${PATH}:/Applications/VMware Fusion.app/Contents/Library"
-#    PROVIDER=vmware_fusion
-# fi
-# vagrant up --provider="${PROVIDER}"
+
+# --- Set up the memory testing box and acquire memory ---
+vagrant up testing --provider="${PROVIDER}"
+VMX="$(cat .vagrant/machines/testing/${PROVIDER}/id)"
+
+if [[ -z "$(vmrun listSnapshots "${VMX}" | grep keys_and_python)" ]]; then
+    vmrun snapshot "${VMX}" keys_and_python
+else
+    echo "Snapshot for memory testing already exists."
+fi
+SNAPSHOT="$(echo ${VMX} | sed "s/\.vmx$"/-Snapshot1.vmem/g)"
 
 
-# # --- Acquire memory ---
-# VMX="$(cat .vagrant/machines/default/${PROVIDER}/id)"
-# if [[ -z "$(vmrun listSnapshots "${VMX}" | grep keys_and_python)" ]]; then
-#     vmrun snapshot "${VMX}" keys_and_python
-# fi
-
-# SNAPSHOT="$(echo ${VMX} | sed "s/\.vmx$"/-Snapshot1.vmem/g)"
-
-# # --- Setup volatility ---
+# --- Test SSH keys ---
+echo $(dirname `pwd`)/plugins
+vol.py --plugins=profiles:"$(dirname `pwd`)/plugins" --profile=LinuxUbuntu1404x64 -f "${SNAPSHOT}" linux_ssh_keys --dump-dir .
